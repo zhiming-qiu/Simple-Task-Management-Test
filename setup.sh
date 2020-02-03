2246e662693705d27d7eca24635a47d5570fcade
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-02-03 22:43:49 +0800

    Debug

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@ git clone https://github.com/zhiming-qiu/Simple-Task-Management-Web-Server.git
 cd Simple-Task-Management-Web-Server
 python -m SimpleHTTPServer 8000 &
 wspid="$!"
-echo $wspid > wspid
-ls
-cat wspid
+echo $wspid > ../wspid
 sleep 10
--- teardown.sh
+++ teardown.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 curl http://127.0.0.1:8000
-ls
-cat wspid
 kill -9 `cat wspid`
