c6ace208b2a520837d7013a200e69ab6a9473254
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-02-03 22:24:16 +0800

    Fix path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone https://github.com/zhiming-qiu/Simple-Task-Management-Web-Server.git
-cd ../Simple-Task-Management-Web-Server
+cd Simple-Task-Management-Web-Server
 python -m SimpleHTTPServer 8000 &
 wspid="$!"
 sleep 10
