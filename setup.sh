b8968cdc39ba11ad58c465e6fb66ded67b990edf
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-02-03 22:21:27 +0800

    Terminate service when needed

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 git clone https://github.com/zhiming-qiu/Simple-Task-Management-Web-Server.git
 cd ../Simple-Task-Management-Web-Server
 python -m SimpleHTTPServer 8000 &
+wspid="$!"
+sleep 10
 curl http://127.0.0.1:8000
+kill -9 $wspid
