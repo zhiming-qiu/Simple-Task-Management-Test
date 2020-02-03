4c37b4d7a090224b4bf1c29ad98d9e37ca5626c2
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-02-03 22:37:32 +0800

    Breakdown workflow steps

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ git clone https://github.com/zhiming-qiu/Simple-Task-Management-Web-Server.git
 cd Simple-Task-Management-Web-Server
 python -m SimpleHTTPServer 8000 &
 wspid="$!"
+echo $wspid > wspid
 sleep 10
-curl http://127.0.0.1:8000
-kill -9 $wspid
